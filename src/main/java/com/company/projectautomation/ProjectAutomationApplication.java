7a7eacc11abcce632436d636e15700b64ecf49b8
Vikas Sanap <vsanap@qualys.com>
2022-09-21 08:41:07 +0530

    initial commit

---
+++ src/main/java/com/company/projectautomation/ProjectAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.company.projectautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/projectautomation/ProjectAutomationApplicationTests.java
@@ -0,0 +1,14 @@
+package com.company.projectautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("hello world");
+	}
+
+}
